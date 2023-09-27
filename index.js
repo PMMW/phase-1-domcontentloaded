a947f64e5d7dd55f3fbe8e8cef8b52e0a56973cb
Morrison Wanja <morrisonwanja@gmail.com>
2023-09-27 20:19:19 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",(e) => {
+    const change =document.getElementById("text");
+    change.textContent ="This is really cool!";
+})
